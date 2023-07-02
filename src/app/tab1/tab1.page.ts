cc5e065f973931ad54ac6d3ca350e8eb48dfafdf
Guilherme Lipi <guigolix28@gmail.com>
2023-07-02 13:40:29 -0300

    a

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NavController } from '@ionic/angular';
+import { ViacepProvider} from './viacep/viacep';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule],
 })
 export class Tab1Page {
-  constructor() {}
+
+  private cep;
+	private endereco:any = {};
+	vazio = "";
+
+  constructor(public navCtrl: NavController, private viacep: ViacepProvider) {}
+
+	getEndereco(){
+		this.viacep.callService(this.cep)
+		.subscribe(
+        data =>{
+		this.endereco = data;
+		console.log(data);
+        }
+      );
+	}
+
+
 }
+
