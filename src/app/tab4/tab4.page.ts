b9ea50a115ca2db6806cfae332f7e119447d7c3c
Guilherme Lipi <guigolix28@gmail.com>
2023-07-02 21:36:01 -0300

    pronto

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class Tab4Page {
-  constructor() {}
+
+  selectedDate: string = '';
+  events: { date: string, description: string }[] = [];
+
+  constructor() { }
+
+  showEvent() {
+    if (this.selectedDate) {
+      this.events.push({ date: this.selectedDate, description: 'Consulta marcada!' });
+      this.selectedDate = '';
+    }
+  }
+
 }
+
